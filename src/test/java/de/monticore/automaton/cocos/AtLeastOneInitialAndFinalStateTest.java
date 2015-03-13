046c34449b8f7f029e45a9dfc3184fb073dde9c9
heim <heim@3ddcf219-3c89-4d16-bbf6-790a9aa18b9b>
2015-03-13 16:37:10 +0000

    use checker of language
    git-svn-id: https://sselab.de/lab2/private/svn/MontiCore/trunk/mclang/automaton@22333 3ddcf219-3c89-4d16-bbf6-790a9aa18b9b

--- src/test/java/de/monticore/automaton/cocos/AtLeastOneInitialAndFinalStateTest.java
+++ src/test/java/de/monticore/automaton/cocos/AtLeastOneInitialAndFinalStateTest.java
@@ -5,66 +5,63 @@
  */
 package de.monticore.automaton.cocos;
 
-import com.google.common.base.Optional;
+import static org.junit.Assert.assertTrue;
 
-import de.monticore.automaton.AbstractTest;
-import de.monticore.automaton._ast.ASTAutomaton;
-import de.monticore.automaton._parser.AutomatonMCParser;
-import de.monticore.cocos.CoCoHelper;
-import de.monticore.cocos.LogMock;
-import de.monticore.cocos.helper.Assert;
-import de.se_rwth.commons.logging.Log;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+
 import mc.ast.SourcePosition;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Collection;
-
-import static org.junit.Assert.assertTrue;
+import de.monticore.automaton.AbstractTest;
+import de.monticore.automaton._ast.ASTAutomaton;
+import de.monticore.automaton._cocos.AutomatonCoCoChecker;
+import de.monticore.cocos.CoCoHelper;
+import de.monticore.cocos.LogMock;
+import de.monticore.cocos.helper.Assert;
+import de.se_rwth.commons.logging.Log;
 
 public class AtLeastOneInitialAndFinalStateTest extends AbstractTest {
-
+  
   @BeforeClass
   public static void init() {
     LogMock.init();
     Log.enableFailQuick(false);
     LogMock.setProduceOutput(false);
   }
-
+  
   @Before
   public void setUp() {
     LogMock.getFindings().clear();
   }
-
+  
   @Test
   public void testValid() throws IOException {
     ASTAutomaton automaton = parseModel("src/test/resources/de/monticore/automaton/cocos/valid/A.aut");
-    AtLeastOneInitialAndFinalState coco = new AtLeastOneInitialAndFinalState();
-    coco.check(automaton);
-
+    AutomatonCoCoChecker checker = AutomatonCoCos.getCheckerForAllCoCos();
+    checker.checkAll(automaton);
+    
     assertTrue(LogMock.getFindings().isEmpty());
   }
-
+  
   @Test
   public void testMissingInitialState() throws IOException {
     ASTAutomaton automaton = parseModel("src/test/resources/de/monticore/automaton/cocos/invalid/MissingInitialState.aut");
-
-    AtLeastOneInitialAndFinalState coco = new AtLeastOneInitialAndFinalState();
-    coco.check(automaton);
-
+    
+    AutomatonCoCoChecker checker = AutomatonCoCos.getCheckerForAllCoCos();
+    checker.checkAll(automaton);
+    
     Collection<String> expectedErrors = Arrays.asList(
         CoCoHelper.buildErrorMsg(
             "0xAUT001", "An automaton must have at least one initial and one final state.",
             new SourcePosition(1, 0))
-    );
-
+        );
+    
     Assert.assertErrors(expectedErrors, LogMock.getFindings());
   }
-
+  
 }
--- src/test/java/de/monticore/automaton/cocos/AutomatonCoCosTest.java
+++ src/test/java/de/monticore/automaton/cocos/AutomatonCoCosTest.java
@@ -5,11 +5,7 @@
  */
 package de.monticore.automaton.cocos;
 
-import static org.junit.Assert.assertTrue;
-
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collection;
 
@@ -19,46 +15,40 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.google.common.base.Optional;
-
 import de.monticore.automaton.AbstractTest;
 import de.monticore.automaton._ast.ASTAutomaton;
 import de.monticore.automaton._cocos.AutomatonCoCoChecker;
-import de.monticore.automaton._parser.AutomatonMCParser;
 import de.monticore.cocos.CoCoHelper;
 import de.monticore.cocos.LogMock;
 import de.monticore.cocos.helper.Assert;
 import de.se_rwth.commons.logging.Log;
 
 public class AutomatonCoCosTest extends AbstractTest {
-
+  
   @BeforeClass
   public static void init() {
     LogMock.init();
     Log.enableFailQuick(false);
     LogMock.setProduceOutput(false);
   }
-
+  
   @Before
   public void setUp() {
     LogMock.getFindings().clear();
   }
-
+  
   @Test
   public void testWithChecker() throws IOException {
     ASTAutomaton automaton = parseModel("src/test/resources/de/monticore/automaton/cocos/invalid"
         + "/StateDoesNotStartWithCapitalLetter.aut");
     
-    // checker and cocos
-    AutomatonCoCoChecker checker = new AutomatonCoCoChecker();
-    checker.addCoCo(new AtLeastOneInitialAndFinalState());
-    checker.addCoCo(new StateNameStartsWithCapitalLetter());
-
+    AutomatonCoCoChecker checker = AutomatonCoCos.getCheckerForAllCoCos();
+    
     Collection<String> expectedErrors = Arrays.asList(
         CoCoHelper.buildErrorMsg("0xAUT002", "State name 'notCapital' should start with a capital "
-                + "letter.",
+            + "letter.",
             new SourcePosition(3, 2))
-    );
+        );
     checker.checkAll(automaton);
     Assert.assertErrors(expectedErrors, LogMock.getFindings());
   }
