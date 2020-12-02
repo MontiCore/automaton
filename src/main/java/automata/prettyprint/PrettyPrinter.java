ebb5cc03e4c8bfa36023195bdc798ace7e905f81
Kautz <kautz@se-rwth.de>
2020-12-02 12:36:54 +0100

    migrated PP to new visitor infrastructure

--- src/main/java/automata/prettyprint/PrettyPrinter.java
+++ src/main/java/automata/prettyprint/PrettyPrinter.java
@@ -4,24 +4,35 @@ package automata.prettyprint;
 import automata._ast.ASTAutomaton;
 import automata._ast.ASTState;
 import automata._ast.ASTTransition;
-import automata._visitor.AutomataVisitor;
+import automata._visitor.AutomataHandler;
+import automata._visitor.AutomataTraverser;
+import automata._visitor.AutomataTraverserImplementation;
+import de.monticore.prettyprint.IndentPrinter;
 
 /**
  * Pretty prints automatons. Use {@link #print(ASTAutomaton)} to start a pretty
  * print and get the result by using {@link #getResult()}.
  *
  */
-public class PrettyPrinter implements AutomataVisitor {
-  private String result = "";
-  
-  private int indention = 0;
-  
-  private String indent = "";
-  
+public class PrettyPrinter implements AutomataHandler {
+  private final IndentPrinter printer;
+  AutomataTraverser traverser;
+
+  public PrettyPrinter() {
+    this.printer = new IndentPrinter();
+    this.traverser = new AutomataTraverserImplementation();
+    traverser.setAutomataHandler(this);
+  }
+
+  public PrettyPrinter(IndentPrinter printer, AutomataTraverser traverser) {
+    this.printer = printer;
+    this.traverser = traverser;
+  }
+
   /**
-   * Prints the automata
+   * Prints the automaton.
    *
-   * @param automaton
+   * @param automaton The input automaton.
    */
   public void print(ASTAutomaton automaton) {
     handle(automaton);
@@ -33,73 +44,64 @@ public class PrettyPrinter implements AutomataVisitor {
    * @return the result of the pretty print.
    */
   public String getResult() {
-    return this.result;
+    return this.printer.getContent();
   }
-  
-  @Override
-  public void visit(ASTAutomaton node) {
-    println("automaton " + node.getName() + " {");
-    indent();
+
+  /**
+   * Gets the printer.
+   *
+   * @return the printer of this.
+   */
+  public IndentPrinter getPrinter() {
+    return this.printer;
   }
-  
+
   @Override
-  public void endVisit(ASTAutomaton node) {
-    unindent();
-    println("}");
+  public void handle(ASTAutomaton node) {
+    getPrinter().println("automaton " + node.getName() + " {");
+    getPrinter().indent();
+    getTraverser().traverse(node);
+    getPrinter().unindent();
+    getPrinter().println("}");
   }
   
   @Override
   public void traverse(ASTAutomaton node) {
     // guarantee ordering: states before transitions
-    node.getStateList().stream().forEach(s -> s.accept(getRealThis()));
-    node.getTransitionList().stream().forEach(t -> t.accept(getRealThis()));
+    node.getStateList().forEach(s -> s.accept(getTraverser()));
+    node.getTransitionList().forEach(t -> t.accept(getTraverser()));
   }
   
   @Override
-  public void visit(ASTState node) {
-    print("state " + node.getName());
+  public void handle(ASTState node) {
+    getPrinter().print("state " + node.getName());
     if (node.isInitial()) {
-      print(" <<initial>>");
+      getPrinter().print(" <<initial>>");
     }
     if (node.isFinal()) {
-      print(" <<final>>");
+      getPrinter().print(" <<final>>");
     }
-    println(";");
+    getPrinter().println(";");
+    getTraverser().traverse(node);
   }
   
   @Override
-  public void visit(ASTTransition node) {
-    print(node.getFrom());
-    print(" - " + node.getInput() + " > ");
-    print(node.getTo());
-    println(";");
+  public void handle(ASTTransition node) {
+    getPrinter().print(node.getFrom());
+    getPrinter().print(" - " + node.getInput() + " > ");
+    getPrinter().print(node.getTo());
+    getPrinter().println(";");
+    getTraverser().traverse(node);
   }
-  
-  private void print(String s) {
-    result += (indent + s);
-    indent = "";
+
+  @Override
+  public AutomataTraverser getTraverser() {
+    return this.traverser;
   }
-  
-  private void println(String s) {
-    result += (indent + s + "\n");
-    indent = "";
-    calcIndention();
+
+  @Override
+  public void setTraverser(AutomataTraverser traverser) {
+    this.traverser = traverser;
   }
-  
-  private void calcIndention() {
-    indent = "";
-    for (int i = 0; i < indention; i++) {
-      indent += "  ";
-    }
-  }
-  
-  private void indent() {
-    indention++;
-    calcIndention();
-  }
-  
-  private void unindent() {
-    indention--;
-    calcIndention();
-  }
+
 }
