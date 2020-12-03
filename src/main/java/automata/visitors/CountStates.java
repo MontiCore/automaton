16ee621cb53defd0b0884547176cbc3e93e20bb8
Kautz <kautz@se-rwth.de>
2020-12-03 13:42:43 +0100

    changed impl from old visitor to new visitor infrastructure

--- src/main/java/automata/visitors/CountStates.java
+++ src/main/java/automata/visitors/CountStates.java
@@ -1,22 +1,41 @@
 /* (c) https://github.com/MontiCore/monticore */
 package automata.visitors;
 
+import automata.AutomataMill;
 import automata._ast.ASTState;
+import automata._visitor.AutomataHandler;
+import automata._visitor.AutomataTraverser;
 import automata._visitor.AutomataVisitor;
 
 /**
  * Counts the states of an automata.
- *
  */
-public class CountStates implements AutomataVisitor {
+public class CountStates implements AutomataHandler {
   private int count = 0;
-  
-  @Override
-  public void visit(ASTState node) {
-    count++;
+  private AutomataTraverser traverser;
+
+  public CountStates() {
+    this.traverser = AutomataMill.traverser();
+    traverser.setAutomataHandler(this);
   }
-  
+
+  @Override
+  public void handle(ASTState node) {
+    count++;
+    getTraverser().traverse(node);
+  }
+
   public int getCount() {
     return count;
+  }
+
+  @Override
+  public AutomataTraverser getTraverser() {
+    return this.traverser;
+  }
+
+  @Override
+  public void setTraverser(AutomataTraverser traverser) {
+    this.traverser = traverser;
   }
 }
